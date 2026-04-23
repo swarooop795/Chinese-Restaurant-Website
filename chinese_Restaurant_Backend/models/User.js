caf4344e596e0b6e1bdad9d797df3eac91b340c1
Swaroopnairy795 <152312261+swarooop795@users.noreply.github.com>
2026-04-23 21:41:13 +0530

    Delete chinese_Restaurant_Backend directory

--- chinese_Restaurant_Backend/models/User.js
+++
@@ -1,1 +0,0 @@
-const mongoose = require('mongoose'); const UserSchema = new mongoose.Schema({  name: String,  email: { type: String, unique: true },  password: String,  role: { type: String, default: "user" }}); nmodule.exports = mongoose.model('User', UserSchema);
