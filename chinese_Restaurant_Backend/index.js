85e4fa6b624f390fa3433633bf68d72d7b0a2a98
Swaroopnairy795 <152312261+swarooop795@users.noreply.github.com>
2026-05-18 14:19:10 +0530

    Update index.js

--- chinese_Restaurant_Backend/index.js
+++ chinese_Restaurant_Backend/index.js
@@ -10,10 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+MONGO_URI = mongodb+srv://Swaroop:Swaroop123@cluster0.abcd.mongodb.net/golden_dragon?retryWrites=true&w=majority
 const JWT_SECRET = "mySuperSecretKey123";
 
 // ================== DATABASE ==================
-mongoose.connect("mongodb://127.0.0.1:27017/golden_dragon")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
