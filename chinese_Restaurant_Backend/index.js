521af0726dd26d75fbc0e2cf89ef7ef256278ffd
Swaroopnairy795 <152312261+swarooop795@users.noreply.github.com>
2026-05-18 14:05:30 +0530

    Update index.js

--- chinese_Restaurant_Backend/index.js
+++ chinese_Restaurant_Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const JWT_SECRET = "mySuperSecretKey123";
 
 // ================== DATABASE ==================
