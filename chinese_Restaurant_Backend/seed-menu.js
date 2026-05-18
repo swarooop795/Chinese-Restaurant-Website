7f8d94c0151da75db80548e159f0aee1f214f87c
Swaroopnairy795 <152312261+swarooop795@users.noreply.github.com>
2026-05-18 15:46:26 +0530

    Update seed-menu.js

--- chinese_Restaurant_Backend/seed-menu.js
+++ chinese_Restaurant_Backend/seed-menu.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Food = require('./models/Food'); // Will be auto-created from schema
 
-mongoose.connect('mongodb://127.0.0.1:27017/golden_dragon')
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
   .then(async () => {
     console.log('Connected to MongoDB');
     
